04b33be4521c25e545df6bbd8e44ad14f0c9a15c
Marina <marinxrt@gmail.com>
2025-03-12 20:39:12 +0100

    add user-menu, info y gallery

--- rallyFotografico/src/app/app.routes.ts
+++ rallyFotografico/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { UserMenuComponent } from './components/usuario/user-menu/user-menu.component';
+import { UserInfoComponent } from './components/usuario/user-info/user-info.component';
+import { UserGalleryComponent } from './components/usuario/user-gallery/user-gallery.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home', component: UserMenuComponent
+    },
+    {
+        path: 'informacion', component: UserInfoComponent
+    },
+    {
+        path: 'gallery', component: UserGalleryComponent
+    }
+];
---
+++ rallyFotografico/src/app/components/usuario/user-gallery/user-gallery.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-gallery',
+  imports: [],
+  templateUrl: './user-gallery.component.html',
+  styleUrl: './user-gallery.component.css'
+})
+export class UserGalleryComponent {
+
+}
